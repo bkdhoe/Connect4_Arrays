180f30ba070983faecbbcb6a58a0f43df55974f6
tgfisher4 <49082176+tgfisher4@users.noreply.github.com>
2019-04-25 10:59:09 -0400

    Update countI.m

--- countI.m
+++ countI.m
@@ -1,4 +1,6 @@
 function num = countI(arr, piece)
+% Given an array and a value, return the number of times 
+% the value appears in the array.
 num = 0;
 for i = 1:length(arr)
         if(arr(i) == piece)
